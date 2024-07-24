e9d9e821cdc0ac639d54cf8ea73a8e197ea45d71
zijie zhong <z22zhong@uwaterloo.ca>
2024-07-24 00:15:11 -0400

    sing

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,31 +1,5 @@
-// src/app/(auth)/sign-in/page.tsx
-"use client"
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { SignIn, useAuth } from '@clerk/nextjs';
+import { SignIn } from '@clerk/nextjs'
 
-const SignInPage = () => {
-  const { isSignedIn } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (isSignedIn) {
-      // 获取重定向URL或默认重定向到dashboard
-      const redirectUrl = router.query.redirect_url ? decodeURIComponent(router.query.redirect_url as string) : '/dashboard';
-      // 确保不重定向到当前页面
-      if (redirectUrl !== '/sign-in') {
-        router.push(redirectUrl);
-      } else {
-        router.push('/dashboard');
-      }
-    }
-  }, [isSignedIn, router]);
-
-  return (
-    <div>
-      <SignIn />
-    </div>
-  );
-};
-
-export default SignInPage;
+export default function Page() {
+    return <SignIn />
+  }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,31 +1,7 @@
-// src/app/(auth)/sign-in/page.tsx
-"use client"
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { SignUp, useAuth } from '@clerk/nextjs';
+//C:\Users\19892\Desktop\automation\src\app\(auth)\sign-up\[[...sign-up]]\page.tsx
+import {  SignUp } from '@clerk/nextjs'
 
-const SignInPage = () => {
-  const { isSignedIn } = useAuth();
-  const router = useRouter();
 
-  useEffect(() => {
-    if (isSignedIn) {
-      // 获取重定向URL或默认重定向到dashboard
-      const redirectUrl = router.query.redirect_url ? decodeURIComponent(router.query.redirect_url as string) : '/dashboard';
-      // 确保不重定向到当前页面
-      if (redirectUrl !== '/sign-in') {
-        router.push(redirectUrl);
-      } else {
-        router.push('/dashboard');
-      }
-    }
-  }, [isSignedIn, router]);
-
-  return (
-    <div>
-      <SignUp />
-    </div>
-  );
-};
-
-export default SignInPage;
+export default function Page() {
+  return <SignUp />
+}
